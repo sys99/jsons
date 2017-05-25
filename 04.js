504f8785b20f850407e4317b9bcd11ac51ad1e55
Luis Roberto Flores <sys99+git@mm.st>
2017-05-25 16:57:43 -0600

    Create 04.js

---
+++ 04.js
@@ -0,0 +1,56 @@
+{
+            "id": "03",
+            "nombre": "Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.",
+            "proyecto": "",
+            "contrato": {
+                "id": "CO01",
+                "nombre": "Contrato de soporte platinum"
+            },
+            "cliente": {
+                "id": "C01",
+                "nombre": "Ministerio de Hacienda",
+                "solicitante": {
+                    "id": "SOL01",
+                    "nombre": "Héctor Dorath"
+                }
+            },
+            "descripcion": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.",
+            "ingeniero": {
+                "id": "MI6382",
+                "nombre": "Edson Artiga"
+            },
+            "producto": {
+                "id": "PROD02",
+                "nombre": "Linux Redhat Enterprise"
+            },
+            "severidad": 3,
+            "actualizaciones": [{
+                    "id": "C01",
+                    "fecha": "12-05-2017",
+                    "autor": "Edson Artiga",
+                    "descripcion": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi."
+                },
+                {
+                    "id": "C02",
+                    "fecha": "13-05-2017",
+                    "autor": "Edson Artiga",
+                    "descripcion": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi."
+                },
+                {
+                    "id": "C03",
+                    "fecha": "14-05-2017",
+                    "autor": "Edson Artiga",
+                    "descripcion": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi."
+                },
+                {
+                    "id": "C04",
+                    "fecha": "15-05-2017",
+                    "autor": "Edson Artiga",
+                    "descripcion": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi."
+                }
+            ],
+            "fecha": "12-05-2017",
+            "fechaCierre": "15-05-2017",
+            "horasActivas": 8,
+            "estado": "pendiente"
+        }
